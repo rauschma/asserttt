0c57affefe86cd204f394ee1e2afb4a4e2a42c37
Axel Rauschmayer <axel@rauschma.de>
2025-01-28 14:19:22 +0100

    Minor updates

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 * Inspiration: https://github.com/TypeStrong/ts-expect
 * https://github.com/type-challenges/type-challenges/blob/main/utils/index.d.ts
+
+### Using the utility types in a TypeScript Playground
+
+```ts
+import type { Equal, Expect } from '@type-challenges/utils';
+```
--- src/assert-type.ts
+++ src/assert-type.ts
@@ -4,21 +4,21 @@
 // • Inspiration: https://github.com/TypeStrong/ts-expect
 // • Feedback welcome!
 
-//==================== Asserting types ====================
+//========== Testing types ==========
 
 // https://github.com/Microsoft/TypeScript/issues/27024#issuecomment-421529650
+// https://stackoverflow.com/questions/53807517/how-to-test-if-two-types-are-exactly-the-same
 export type Equal<X, Y> =
   (<T>() => T extends X ? 1 : 2) extends
   (<T>() => T extends Y ? 1 : 2) ? true : false;
 
-// Is `Target` assignable from `Source`?
-export type Assignable<Target, Source> = Source extends Target ? true : false;
+export type Extends<Target, Source> = Source extends Target ? true : false;
 
-// Credit: Blaine Bublitz
+// Credit: based on code by Blaine Bublitz
 export type Assert<_T extends true> = void;
 
 // ❌ Would be useful – but I don’t see how:
-// @ts-expect-error: Type 'Equal<X, Y>' does not satisfy the constraint 'true'.
+// Type 'Equal<X, Y>' does not satisfy the constraint 'true'.
 export type AssertEqual<X, Y> = Assert<Equal<X, Y>>;
 
 // Is the type of `_value` assignable to `T`?
@@ -26,14 +26,18 @@ export function assertType<T>(_value: T): void {}
 
 export type Not<B extends boolean> = B extends true ? false : true;
 
-//---------- Usage ----------
+// Expand name to type information
+// Source: https://github.com/type-challenges/type-challenges/blob/main/utils/index.d.ts
+export type Debug<T> = { [K in keyof T]: T[K] };
 
-type _1 = Assert<Assignable<number, 123>>;
+//----- Usage -----
+
+type _1 = Assert<Extends<number, 123>>;
 
 type Pair<X> = [X, X];
 type _2 = Assert<Equal<Pair<'a'>, ['a', 'a']>>;
 
-//==================== Simple assertions for non-test code ====================
+//========== Simple assertions for non-test code ==========
 
 /**
  * Useful whenever you don’t want to use Node’s built-in `assert()` or
@@ -52,7 +56,7 @@ export function assertNonNullable<T>(value: T, message?: string): asserts value 
   }
 }
 
-//==================== Exhaustiveness checking ====================
+//========== Exhaustiveness checking ==========
 
 export class UnsupportedValueError extends Error {
   constructor(value: never, message = `Unsupported value: ${String(value)}`) {
@@ -60,7 +64,7 @@ export class UnsupportedValueError extends Error {
   }
 }
 
-//---------- Usage ----------
+//----- Usage -----
 
 enum Color {Red, Green}
 function colorToString(color: Color) {
